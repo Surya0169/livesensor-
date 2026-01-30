178954c035b579c1881661ef288fe0fa094be5e0
Shourya Singh <shouryasingh@Shouryas-MacBook-Air.local>
2026-01-30 14:32:47 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages ,setup
+#from typing import list 
+#from typing import List
+
+
+
+
+
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="prince",
+    author_email="anupstxavier12@gmail.com",
+    packages=find_packages(),
+    install_requires = ["pymongo"]
+
+)
